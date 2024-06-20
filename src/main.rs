772fbfc84c10736fc07df3752cea2eca0d7bd8ea
LiamFarese <108936972+LiamFarese@users.noreply.github.com>
2024-06-20 22:40:46 +0100

    Integration tests and application

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-fn test() {
-    println!("Testing");
-}
-fn main() {
-    println!("Hello, world!");
-    test();
+use std::net::TcpListener;
+use zero2prod::run;
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener : TcpListener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
